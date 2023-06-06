c282588beda5ab8b194375c69f3cac3ac8c97730
Julierme-Caneppele <dudi_c22@hotmail.com>
2023-06-06 00:12:35 +0000

    arquivo gasolina.py alterado em develop

--- gasolina.py
+++ gasolina.py
@@ -4,3 +4,12 @@ gasolina = data[['dia', 'venda']].reset_index()
 gasolina.head()
 grafico = sns.barplot(data=gasolina, x='dia', y='venda', ci=None, palette='pastel')
 grafico.set(title='Preço da gasolina', xlabel='Dia', ylabel= 'Venda');
+
+import os
+
+if not os.path.exists('gasolina.py'):
+    os.makedirs('gasolina.py')
+
+ax = grafico = sns.barplot(data=gasolina, x='dia', y='venda', ci=None, palette='pastel')
+fig = ax.get_figure()
+fig.savefig('preco_da_gasolina.png')
